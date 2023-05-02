fb0e351ace38f598c8f68180b586bcf39856e7c7
davidvoetterl <david.voetterl12@gmail.com>
2023-05-02 21:18:55 +0200

    Add existing project files to Git

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,9 @@
 import argparse
 
-#def welcome():
-  #  print("Willkommen beim IMI Taschenrechner")
-    #calcoperator = input("Bitte geben Sie eins der folgenden vier Zeichen ein, um die Rechenoperation zu bestimmen: + - / *\n")
-
-#def addition(a,b):
-  #  result =  a + b
-    #return result
-
-#def substraction(a,b):
-  #  result =  a - b
-    #return result
-
-#def division(a,b):
-  #  result =  a / b
-   # return result
-
-#def multiplication(a,b):
-  #  result =  a * b
-   # return result
-
-def main():
-
+def parse():
 
     parser = argparse.ArgumentParser(description='IMI Taschenrechner')
-    parser.add_argument('operation', help='addition, subtraction, division or multiplication')
+    parser.add_argument('operation', help='addition, subtraction, division or multiplication', type=str)
     parser.add_argument('number1', type=int)
     parser.add_argument('number2', type=int)
 
@@ -33,13 +12,24 @@ def main():
     a = args.number1
     b = args.number2
 
-    if args.operation == "addition":
-        print (a+b)
-    if args.operation == "subtraction":
-        print (a-b)
-    if args.operation == "division":
-        print (a/b)
-    if args.operation == "multiplication":
-        print (a*b)
+    operationtyp = args.operation
 
-main()
+def calculation():
+
+    if operationtyp == "addition":
+        return (a+b)
+    if operationtyp == "subtraction":
+        return (a-b)
+    if operationtyp == "division":
+        return (a/b)
+    if operationtyp == "multiplication":
+        return (a*b)
+
+result = calculation()
+
+def display():
+    print("The result is: " + result)
+
+parse()
+calculation()
+display()
